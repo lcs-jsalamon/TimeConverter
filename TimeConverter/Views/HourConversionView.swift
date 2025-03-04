58e8c1ebca6f12430c97e8962fcae34e05cbeddf
Josephine Salamon <jsalamon@lcs.on.ca>
2025-03-04 12:52:12 -0500

    got input working with appropriate output

--- TimeConverter/Views/HourConversionView.swift
+++ TimeConverter/Views/HourConversionView.swift
@@ -10,10 +10,29 @@ import SwiftUI
 struct HourConversionView: View {
     
     //MARK: Stored Properties
+    @State var viewModel = HourConversionViewModel()
     
     //MARK: Computed Properties
     var body: some View {
-        Text("Hello World")
+        VStack{
+            //1. show our result if one exsits
+            if let conversionResult = viewModel.conversionResult {
+                
+                Text("\(conversionResult.timeInHours)")
+                Text("\(conversionResult.timeInSeconds)")
+                Text("\(conversionResult.timeInMinutes)")
+                
+                
+                
+            } else {
+                //2. show the content unavaible message
+                ContentUnavailableView("Unable to convert", systemImage: "gear.badge.questionmark", description: Text("\(viewModel.recoverySuggestion)"))
+                
+            }
+            
+            // 3. allow for input to occur
+            TextField("Enter the time in hours", text: $viewModel.providedHours)
+        }
     }
 }
 

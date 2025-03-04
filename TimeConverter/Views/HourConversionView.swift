d72242e286465dd651954e4587f748bb047fdf44
Josephine Salamon <jsalamon@lcs.on.ca>
2025-03-04 12:38:32 -0500

    Initial Commit

---
+++ TimeConverter/Views/HourConversionView.swift
@@ -0,0 +1,18 @@
+//
+//  HourConversionView.swift
+//  TimeConverter
+//
+//  Created by Russell Gordon on 2025-02-24.
+//
+
+import SwiftUI
+
+struct HourConversionView: View {
+    
+    //MARK: Stored Properties
+    
+    //MARK: Computed Properties
+    var body: some View {
+    
+    }
+}
